d68ee6b18d6d7e4789a03141ed42d89f34cbedc0
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-04-22 14:51:34 -0300

    feat & refact:  get hotels with id

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -3,19 +3,20 @@ import { NextFunction, Response } from 'express';
 import { AuthenticatedRequest } from '@/middlewares';
 import hotelService from '@/services/hotels-service';
 
-export async function getHotels(req: AuthenticatedRequest, res: Response) {
+export async function getHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const { userId } = req;
   try {
     const hotels = await hotelService.getAll(userId);
     return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
-    if (error.name === 'NotFoundError') {
-      return res.sendStatus(httpStatus.NOT_FOUND);
-    } else if (error.name === 'RequiredPaymentError') {
-      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
-    } else {
-      return res.sendStatus(httpStatus.BAD_REQUEST);
-    }
+    // if (error.name === 'NotFoundError') {
+    //   return res.sendStatus(httpStatus.NOT_FOUND);
+    // } else if (error.name === 'RequiredPaymentError') {
+    //   return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    // } else {
+    //   return res.sendStatus(httpStatus.BAD_REQUEST);
+    // }
+    next(error);
   }
 }
 
